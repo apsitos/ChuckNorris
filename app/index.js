1a7e4884b8de60400afd59ad2a641fd2dda74e56
Annastasia Psitos <annastasia.psitos@gmail.com>
2017-01-26 13:41:12 -0700

    add Initial child to Router, reorganize children

--- app/index.js
+++ app/index.js
@@ -2,19 +2,22 @@ import React from 'react';
 import { render } from 'react-dom';
 import Main from './components/Main/Main';
 import {Initial} from './components/Initial/Initial';
-import Joke from './components/Jokes/Jokes';
+import Jokes from './components/Jokes/Jokes';
 // import Settings from './components/Settings/Settings';
 // import Favorites from './components/Favorites/Favorites';
 import { Router, hashHistory, Route, IndexRoute } from 'react-router';
+
+{/* <Route path="/settings" component={Settings} />
+<Route path="/favorites" component={Favorites} /> */}
+
+{/* <IndexRoute component={Joke} /> */}
 
 
 render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
-      <Route path="/jokes" component={Joke} />
-        {/* <IndexRoute component={Joke} /> */}
-      {/* <Route path="/settings" component={Settings} /> */}
-      {/* <Route path="/favorites" component={Favorites} /> */}
+      <IndexRoute component={Initial} />
+      <Route path="/jokes" component={Jokes} />
     </Route>
   </Router>
   , document.getElementById('application'));
